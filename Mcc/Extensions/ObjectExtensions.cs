7dda4a9b2ca5acfb10a4f6652d5fb8d52ae2fa9c
Rolf Lutgendorff <rolf_lutgendorff@hotmail.com>
2023-03-02 20:39:10 +0100

    add extension method for object to json

---
+++ Mcc/Extensions/ObjectExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Mcc.Extensions;
+
+public static class ObjectExtensions
+{
+    public static string ToJson(this object obj)
+    {
+        return System.Text.Json.JsonSerializer.Serialize(obj);
+    }
+}
