bb949b9c50958631e6e83c45e22207c7de149a73
Rolf Lutgendorff <rolf_lutgendorff@hotmail.com>
2023-03-08 22:00:22 +0100

    added helper classes

---
+++ Mcc.EventSourcing/TypeConverter.cs
@@ -0,0 +1,11 @@
+﻿namespace Mcc.EventSourcing;
+
+public class TypeConverter : ITypeConverter
+{
+    public Type? CreateType(string type)
+    {
+        var createdType = Type.GetType(type);
+
+        return createdType;
+    }
+}
---
+++ Mcc/Caster.cs
@@ -0,0 +1,11 @@
+﻿namespace Mcc;
+
+public static class Caster
+{
+    public static TTo EnumToEnum<TFrom, TTo>(TFrom input)
+        where TFrom : Enum
+        where TTo : Enum
+    {
+        return (TTo)System.Enum.Parse(typeof(TTo), input.ToString());
+    }
+}
