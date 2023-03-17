94ce734adaed7c55aab7ca46bc789bf9856b5b2b
Rolf Lutgendorff <rolf_lutgendorff@hotmail.com>
2023-03-17 23:30:42 +0100

    added projection util classes

---
+++ Mcc.EventSourcing/Projections/BaseProjectionRepository.cs
@@ -0,0 +1,20 @@
+﻿using Mcc.Repository.Databases;
+
+namespace Mcc.EventSourcing.Projections;
+
+public abstract class BaseProjectionRepository<TEntity> : IProjectionRepository<TEntity>
+{
+    protected readonly IDatabaseRepository<TEntity> Repository;
+
+    protected BaseProjectionRepository(IDatabaseRepository<TEntity> repository)
+    {
+        Repository = repository;
+    }
+
+    public IQueryable<TEntity> Get()
+    {
+        return Repository.GetAll();
+    }
+
+    public abstract Task RebuildAsync(CancellationToken cancellationToken);
+}
---
+++ Mcc.EventSourcing/Projections/ProjectionRebuilder.cs
@@ -0,0 +1,28 @@
+﻿using Mcc.EventSourcing.Stores;
+using Mcc.Repository.Databases;
+
+namespace Mcc.EventSourcing.Projections;
+
+public class ProjectionRebuilder<TEntity> : IProjectionRebuilder<TEntity>
+{
+    private readonly IDatabaseRepository<TEntity> _repository;
+    private readonly IEventStore _eventStore;
+
+    public ProjectionRebuilder(IDatabaseRepository<TEntity> repository, IEventStore eventStore)
+    {
+        _repository = repository;
+        _eventStore = eventStore;
+    }
+
+    public async Task RebuildAsync(Func<EventWrapper, Task> action, CancellationToken cancellationToken)
+    {
+        await _repository.DeleteAsync(_repository.GetAll(), cancellationToken);
+
+        var events = await _eventStore.ReadAllStreams(cancellationToken);
+
+        foreach (var @event in events)
+        {
+            await action(@event);
+        }
+    }
+}
